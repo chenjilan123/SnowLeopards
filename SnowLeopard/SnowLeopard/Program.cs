cd8ea450e919c0abfb7a9ff9826f95cdb32ed8d7
11 <11@DESKTOP-M5DA0GK>
2018-12-07 08:45:11 +0800

    2

--- SnowLeopard/SnowLeopard/Program.cs
+++ SnowLeopard/SnowLeopard/Program.cs
@@ -1,5 +1,4 @@
-﻿using SnowLeopard.WFs;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,15 +8,9 @@ namespace SnowLeopard
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Console.WriteLine("Hehe");
         }
     }
 }
