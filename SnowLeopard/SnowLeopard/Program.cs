ca20ff4de139b0c1187eb570ece238b37aa79f30
11 <11@DESKTOP-M5DA0GK>
2018-12-11 14:53:14 +0800

    1

--- SnowLeopard/SnowLeopard/Program.cs
+++ SnowLeopard/SnowLeopard/Program.cs
@@ -19,9 +19,13 @@ namespace SnowLeopard
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
             //Application.Run(new StartupShotdown());
+<<<<<<< HEAD
             //Application.Run(new BorderTimer());
             //Application.Run(new Align());
             Application.Run(new Layout());
+=======
+            Application.Run(new BorderTimer());
+>>>>>>> parent of bb4c8d9... 1
         }
     }
 }
