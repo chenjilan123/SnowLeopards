1be272024d2bc03a5b6ef3d44adcda47e7554e77
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2018-12-12 22:20:33 +0800

    Revert "1"
    This reverts commit d457603ced6b01e9fbb157d32e47b81eba6dd457.

--- SnowLeopard/SnowLeopard/WFs/Align.Designer.cs
+++ SnowLeopard/SnowLeopard/WFs/Align.Designer.cs
@@ -87,6 +87,7 @@
             this.button2.TabIndex = 0;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // checkBox1
             // 
