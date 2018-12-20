b270ccfd2735748dd703cfd5a146b416fe9805c5
11 <11@DESKTOP-M5DA0GK>
2018-12-20 13:39:29 +0800

    1

---
+++ SnowLeopard/SnowLeopard/Model/Redundancy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnowLeopard.Model
+{
+    public class Redundancy
+    {
+
+    }
+}
