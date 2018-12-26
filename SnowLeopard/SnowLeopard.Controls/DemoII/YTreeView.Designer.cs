ff3a5202924635c8a5ebe466ee1f07810b648904
11 <11@DESKTOP-M5DA0GK>
2018-12-26 12:28:57 +0800

    1

---
+++ SnowLeopard/SnowLeopard.Controls/DemoII/YTreeView.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace SnowLeopard.Controls.DemoII
+{
+    partial class YTreeView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.treeView2 = new CCWin.SkinControl.SkinTreeView();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(8, 39);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(187, 534);
+            this.treeView1.TabIndex = 0;
+            // 
+            // treeView2
+            // 
+            this.treeView2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.treeView2.Location = new System.Drawing.Point(348, 39);
+            this.treeView2.Name = "treeView2";
+            this.treeView2.Size = new System.Drawing.Size(192, 534);
+            this.treeView2.TabIndex = 1;
+            // 
+            // YTreeView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 581);
+            this.Controls.Add(this.treeView2);
+            this.Controls.Add(this.treeView1);
+            this.Name = "YTreeView";
+            this.Text = "YTreeView";
+            this.Load += new System.EventHandler(this.YTreeView_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private CCWin.SkinControl.SkinTreeView treeView2;
+    }
+}
---
+++ SnowLeopard/SnowLeopard.Controls/DemoII/YTreeView.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnowLeopard.Controls.DemoII
+{
+    public partial class YTreeView : BlueForm
+    {
+        private readonly TreeView[] trees = null;
+        public YTreeView()
+        {
+            InitializeComponent();
+            trees = new[]
+            {
+                treeView1,
+                treeView2,
+            };
+            //控件未创建句柄时，可以异步加载
+            InitTree();
+        }
+
+        private async void InitTree()
+        {
+            await Task.Run(() =>
+            {
+                foreach (var tree in trees)
+                {
+                    InitTree(tree);
+                }
+            });
+        }
+
+        private void YTreeView_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitTree(TreeView tree)
+        {
+            tree.Nodes.Add(new TreeNode() { Text = "Root", Name = "Root" });
+
+            AddChild((TreeNode)tree.Nodes[0], 5);
+        }
+
+        private void AddChild(TreeNode node, int count)
+        {
+            while (count-- > 0)
+            {
+                node.Nodes.Add($"{node.Name}_{count}");
+            }
+        }
+    }
+}
