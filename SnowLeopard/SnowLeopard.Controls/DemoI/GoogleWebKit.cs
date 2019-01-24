05a976a3677cbac181359cce7e40cbde9e1a92f1
11 <11@DESKTOP-M5DA0GK>
2019-01-24 11:44:43 +0800

    1

--- SnowLeopard/SnowLeopard.Controls/DemoI/GoogleWebKit.cs
+++ SnowLeopard/SnowLeopard.Controls/DemoI/GoogleWebKit.cs
@@ -22,13 +22,13 @@ namespace SnowLeopard.Controls.DemoI
             var sTest = "http://liulanmi.com/labs/core.html";
             var sAuth = "https://cloud.roadefend.com/loginRDFEmbedded.action?username=zhuser&password=961bd6e205c7accf70be384161a3aae2b8bb2104e93642393e578369172b13ef47461a67f4a95770f58f7ab88dc3111c6a199a001ae29a5c53208681b4158c1c";
 
-            webKitBrowser2.Url = new Uri(sAuth);
+            webKitBrowser2.Url = new Uri("http://www.baidu.com");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             var sPage = "https://cloud.roadefend.com/";
-            webKitBrowser2.Url = new Uri(sPage);
+            //webKitBrowser2.Url = new Uri(sPage);
         }
     }
 }
